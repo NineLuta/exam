01d14ca1bf28d52b1e5deb7d70230019842296a4
NineLuta <oda81157@gmail.com>
2024-09-02 01:29:04 +0300

    sec commit

--- admin/view/layout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../view/header.php";
-include "../view/sidebar.php";
-include "../view/body.php";
-include "../view/navbar.php";
-include "../view/footer.php";
-
--- inc/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-sESSION_start();
-
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="website";
-$conn=mysqli_connect($host,$dbuser, $dbpass, $db);
-?>
